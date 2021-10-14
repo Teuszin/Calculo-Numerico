d14cd29010e50b370914c2e8e2a8f603788a2d18
Mate._.Henri <mateus--henrique@hotmail.com>
2021-10-14 01:14:04 -0300

    lista 2 newton

---
+++ Listas_do_Lop/Lista_02/Raiz_M_Newton.py
@@ -0,0 +1,27 @@
+import math
+
+
+def f(A,B,C,D,x):
+    return A*math.pow(x,3) + B*math.pow(x,2) + C*x + D
+
+def df(A,B,C,D,x):
+    h = 1e-8
+    df = (f(A,B,C,D,x+h) - f(A,B,C,D,x)) / h
+    return df
+
+A = float(input())
+B = float(input())
+C = float(input())
+D = float(input())
+X0 = float(input())
+newton = 0
+interacoes = 1
+
+while abs(f(A,B,C,D,X0)) > 0.0001:
+    newton = X0 - f(A,B,C,D,X0) / df(A,B,C,D,X0)
+    X0 = newton
+    interacoes += 1
+    if interacoes >= 100:
+        break
+
+print(f'{X0:0.3f} {interacoes}')
